3c4e358d95195214acb0ef4ee4ef26e4a5f92fa7
Jasper Butcher <JasperKBV@gmail.com>
2023-10-27 21:05:32 +0100

    Added files

--- main.py
+++ main.py
@@ -10,119 +10,46 @@ from ase.io import read, write
 # ML / Linalg
 import numpy as np
 
+# OAI
+import openai
+from dotenv import load_dotenv; load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def format_query(past_queries):
+def query_llm(messages=[]):
 
-    sys_msg = """You are an expert computational materials scientist. 
-You are trying to develop an experiment to crystallise carbon into diamond."""
+    response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=messages,
+        temperature=0.1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
 
-    q = """Predict appropriate code for a melt-quench simulation of this system. 
-The format of the code should be a LAMMPS input file.
-A 
-
-# EXAMPLE 1
-RESULT
-SOAP similarity to diamond is 0.9
-SOAP similarity to graphite is 0.1
-
-SCRIPT:
-# Phase 1: Melt
-fix integrate all nvt temp 9000 9000 0.1
-run 100000
-unfix integrate
-
-# Phase 2: Quench
-fix integrate all nvt temp 9000 3000 0.1
-run 100000 
-unfix integrate
-
-# Phase 3: Warm
-fix integrate all nvt temp 3000 500 0.1
-run 100000 
-unfix integrate
-
-# Phase 4: Anneal
-fix integrate all nvt temp 500 500 0.1
-run 100000 
-unfix integrate
-
-# Phase 5: Cool
-fix integrate all nvt temp 500 300 0.1
-run 100000
-unfix integrate
-
-# EXAMPLE 2 
-RESULT:
-SOAP similarity to diamond is 1.0
-SOAP similarity to graphite is 0.0
-
-SCRIPT:
-"""
-    
-    ##########
-    # Include a number of past experiments
-    q += ""  #  TODO: format past queries
-    ##########
+    message = response.choices[0].message
+    messages.append(message)
+    code = message['content']
+    print(response)
+    print(messages)
+    print(code)
+    return code, messages, response
     
 
-    return q
-    
-def infer_experiment_phase(query):
-    # Use LLM to infer the next phase of the MD
-    ...
-
-def format_experiment(query):
-
-    # MQ params
-    # timesteps in ps
-    melt=5
-    quench=5
-    warm=50
-    anneal=100
-    cool=50
-
-    # temperatures in K
-    melt_T=9000
-    quench_T=3000
+def format_experiment():
 
     structure_file = abspath("structures/example.extxyz") 
-    dump_file = abspath("dump_file")
+    dump_file = abspath("dump_file")  # no extension
+    log_file = abspath("log.dat")
     pot = abspath("data/carbon.xml")
     rand_seed = 42  
 
-    # Step 1: Declare necessary variables
-    quench = 100
-    warm = 100
-    anneal = 100 
-    cool = 100
-    melt = 100
+    ncomp = 100000  # (1000*nps) in fs
+    compfreq = 100/(ncomp*1000)
+    T_0 = 3000  # initial temperature for velcoity creation
 
-    melt_T = 9000
-    quench_T = 3000
-    anneal_T = 500
-    cool_T = 300
-
-
-    # Step 2: Create LAMMPS input file string
     lammps_input = f"""
-# variable melt_steps equal {melt*1000}
-# variable quench_steps equal {quench*1000}
-# variable warm_steps equal {warm*1000}
-# variable anneal_steps equal {anneal*1000}
-# variable cool_steps equal {cool*1000}
-
-# variable Nfreq equal 100
-# variable Nevery equal 100
-# variable Nrepeat equal {100/(warm*1000)}
-# variable Ndump equal 1000
-
-print "Melt  : {melt*1000}fs"
-print "Quench: {quench*1000}fs"
-print "Warm  : {warm*1000}fs"
-print "Anneal: {anneal*1000}fs"
-print "Cool  : {cool*1000}fs"
-
-log ./log.dat append
+log {log_file} append
 
 units metal     # mass = g/mole, distance = Å, time = ps, energy = eV, force = eV/Å
 atom_style atomic
@@ -140,98 +67,109 @@ variable nAtoms equal atoms
 fix removeMomentum all momentum 1 linear 1 1 1
 
 compute T all temp
-fix TempAve all ave/time {warm*1000} {100/(warm*1000)} 100 c_T
-
 variable P equal press
-fix PressAve all ave/time {warm*1000} {100/(warm*1000)} 100 v_P
-
 variable v equal vol
-fix vAve all ave/time {warm*1000} {100/(warm*1000)} 100 v_v
-
-compute PE all pe pair
-
 variable PE_Atom equal c_PE/v_nAtoms
 
-fix PEAve_Atom all ave/time {warm*1000} {100/(warm*1000)} 100 v_PE_Atom
+fix TempAve all ave/time {ncomp} {compfreq} 100 c_T
+fix PressAve all ave/time {ncomp} {compfreq} 100 v_P
+fix vAve all ave/time {ncomp} {compfreq} 100 v_v
+fix PEAve_Atom all ave/time {ncomp} {compfreq} 100 v_PE_Atom
 
+compute PE all pe pair
 compute MSD all msd
 
 thermo_style custom step cpu temp f_TempAve press f_PressAve f_PEAve_Atom vol f_vAve c_MSD[4]
 thermo_modify flush yes
-thermo {warm*1000}
+thermo {ncomp}
 
-dump traj all atom {warm*1000} {dump_file}.dump.*.dat
+dump traj all atom {ncomp} {dump_file}.dump.*.dat
 
-velocity all create {melt_T} {rand_seed}
+velocity all create {T_0} {rand_seed}
 
 run 0
 
-# Trajectory Phases
-# Phase 1: Melt
-fix integrate all nvt temp {melt_T} {melt_T} 0.1
-run {melt*1000}
-unfix integrate
-
-# Phase 2: Quench
-fix integrate all nvt temp {melt_T} {quench_T} 0.1
-run {quench*1000} 
-unfix integrate
-
-# Phase 3: Warm
-fix integrate all nvt temp {quench_T} {anneal_T} 0.1
-run {warm*1000} 
-unfix integrate
-
-# Phase 4: Anneal
-fix integrate all nvt temp {anneal_T} {anneal_T} 0.1
-run {anneal*1000} 
-unfix integrate
-
-# Phase 5: Cool
-fix integrate all nvt temp {anneal_T} {cool_T} 0.1
-run {cool*1000}
-unfix integrate
-
+# PHASES:
 """
+    # When a full experiment is requested, add some phases to the experiment
+#     if FULL: lammps_input += f"""# Prebuilt Phases
+# # Phase 1: Melt
+# fix integrate all nvt temp {melt_T} {melt_T} 0.1
+# run {melt*1000}
+# unfix integrate
+
+# # Phase 2: Quench
+# fix integrate all nvt temp {melt_T} {quench_T} 0.1
+# run {quench*1000} 
+# unfix integrate
+
+# # Phase 3: Warm
+# fix integrate all nvt temp {quench_T} {anneal_T} 0.1
+# run {warm*1000} 
+# unfix integrate
+
+# # Phase 4: Anneal
+# fix integrate all nvt temp {anneal_T} {anneal_T} 0.1
+# run {anneal*1000} 
+# unfix integrate
+
+# # Phase 5: Cool
+# fix integrate all nvt temp {anneal_T} {cool_T} 0.1
+# run {cool*1000}
+# unfix integrate
+
+# """
     return lammps_input
 
 
 def main():
-    past_queries = [
+
+    messages = [
         {
-            'lammps_phase': '',
-            'metrics': {
-                'soap_similarity': None,
-            }
-        }
+        "role": "system",
+        "content": "You are an expert computational materials scientist. \nYou are trying to develop an experiment to crystallise carbon into diamond.\nPredict appropriate code for a melt-quench simulation of this system. \nTry NOT to use an extremely large number of timesteps.\nThe format of the code should be a LAMMPS input file like:\n# Phase PHASE:\nfix integrate all nvt temp T_INITIAL T_FINAL 0.1\nrun TIMESTEPS\nunfix integrate\n"
+        },
+        {
+        "role": "user",
+        "content": "RESULT:\nSOAP similarity to diamond is 0.1\nSOAP similarity to graphite is 0.9\n\nSCRIPT:"
+        },
+        {
+        "role": "assistant",
+        "content": "# Phase 1: \nfix integrate all nvt temp 9000 9000 0.1\nrun 100000\nunfix integrate\n\n# Phase 2: \nfix integrate all nvt temp 9000 3000 0.1\nrun 100000 \nunfix integrate\n\n# Phase 3: \nfix integrate all nvt temp 3000 500 0.1\nrun 100000 \nunfix integrate\n\n# Phase 4:\nfix integrate all nvt temp 500 500 0.1\nrun 100000 \nunfix integrate\n\n# Phase 5:\nfix integrate all nvt temp 500 300 0.1\nrun 100000\nunfix integrate"
+        },
+        {
+        "role": "user",
+        "content": "RESULT:\nSOAP similarity to diamond is 1.0\nSOAP similarity to graphite is 0.0\n\nSCRIPT:"
+        },
     ]
     while True:
-        # (1) Get updated query
-        query = format_query(query_result)
-        # (2) Infer experiment from query
-        experiment_string = infer_experiment(query)
-        # (3) Run experiment
-        print('submitting experiment: \n\n\n', experiment_string)
-        with open('lammps/experiment.in', 'w') as f:
-            f.write(experiment_string)
-        os.system('qsub submit.sh lammps/experiment.in')
-        # Check SGE queue for job completion
-        atoms = read('')
+        # query llm
+        code, messages, response = query_llm(messages)
 
-        # (4) Compute metrics
-        # (5) Update db
-        break
+        # submit experiment
 
+        # wait
 
+        # analyse results and update system response
+
+        exit()
 
 if __name__ == '__main__':
-    # few tests
-    # x = infer_experiment(query)
-    s = format_experiment(None)
-    print(s)
-    infile_path = '/home/jbutch/Projects/Thesis/llms-for-lammps/lammps/experiment.in'
+    # # TEST FORMATTING
+    # s = format_experiment()
+    # print(s)
+    # infile_path = '/home/jbutch/Projects/Thesis/llms-for-lammps/lammps/experiment_human.in'
+    # with open(infile_path, 'w') as f:
+    #     f.write(s)
+
+    phases_str = query_llm()
+    print(phases_str)
+
+    s = format_experiment(FULL=False)
+    s += phases_str  # append phases to experiment
+    infile_path = '/home/jbutch/Projects/Thesis/llms-for-lammps/lammps/experiment_ai.in'
     with open(infile_path, 'w') as f:
         f.write(s)
+    print(s)
 
-    # main()
-
+    code, messages, response
